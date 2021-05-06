df22b8d445a34aba9ea56eb5b5359a7a4a602e4e
Sebastian <seba.lewy@op.pl>
2021-05-06 20:42:54 +0100

    Add: timer, progress bar, changed img width, button style/disable

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,24 @@
-<h1>Hello</h1>
+<script>
+
+import Timer from "./Timer.svelte";
+
+import HowTo from "./HowTo.svelte"
+
+</script>
+
+
+<style>
+
+h1{
+
+    text-align: center;
+}
+</style>
+
+<h1>Handwoshing App</h1>
+
+<h1 class="nex"> Timer test</h1>
+
+<Timer />
+
+<HowTo />
---
+++ src/HowTo.svelte
@@ -0,0 +1,16 @@
+
+<style>
+
+
+    img{
+
+        max-width: 100%;
+    }
+</style>
+
+
+<div bp="grid">
+    
+    
+    <img src="handwashing.gif" bp = "offset-5@md 4@md 12@sm" 
+    alt="How to wash your hands"></div>
